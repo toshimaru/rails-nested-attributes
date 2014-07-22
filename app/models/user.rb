86e10f247cb21fde4487006c42ff3dff2486420d
toshi <me@toshimaru.net>
2014-07-23 03:39:14 +0900

    Define association between user and todo models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :todos, dependent: :destroy
+
+  accepts_nested_attributes_for :todos
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
