6f6095584e26429ae760793982f7ec26a9e87255
toshimaru <me@toshimaru.net>
2020-05-23 04:14:42 +0900

    Add reject_if to accepts_nested_attributes_for

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :todos, dependent: :destroy
 
-  accepts_nested_attributes_for :todos
+  accepts_nested_attributes_for :todos, reject_if: ->(attributes) { attributes['task'].blank? }
 
   validates :name, presence: true
   validates :email, presence: true
